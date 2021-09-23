6cc060138b41a5b0f78f352be3b4e9b3169488f1
julian francia <julianfrancia110@gmail.com>
2021-09-22 21:25:42 -0300

    cambios necesarios para desafio clase 12

--- serverJulianFrancia.js
+++ serverJulianFrancia.js
@@ -1,23 +1,34 @@
 const express = require('express');
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
 const Productos = require('./productos');
 
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const PORT = 8080;
 const router = express.Router();
+const newRouter = express.Router();
 let listaProductos = []
 const productos = new Productos(listaProductos);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use('/', newRouter);
 app.use('/api', router);
-app.use(express.static('public'));
 
-const server = app.listen(PORT, () => {
+const server = http.listen(PORT, () => {
     console.log(`server escuchando en ${server.address().port}`)
 })
 
 server.on('error',error => console.log(`error en el server: ${error}`));
+
+io.on('connection', (socket) => {
+    socket.emit('mostrarProductos', productos.devolverLista())
+    socket.on('guardarProducto', data => {
+        productos.guardarUnProducto(data);
+        socket.emit('mostrarProductos', productos.devolverLista())
+    });
+})
 
 app.engine(
     "hbs",
@@ -41,7 +52,7 @@ router.get('/productos/listar/:id', (req,res) => {
     res.json(productos.devolveUnProducto(req.params.id))
 });
 
-router.get('/productos/vista', (req, res) => {
+newRouter.get('/', (req, res) => {
     let listExist = productos.devolverLista().length > 0 ? true : false;
     res.render('main',{listaProd: productos.devolverLista(), listExist: listExist, listNotExist: !listExist});
 })
